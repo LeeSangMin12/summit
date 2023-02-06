fa4befdc4b9855d1ab6154eb403c389c959d3df7
LeeSangMin12 <isangmin516@gmail.com>
2023-02-06 15:34:09 +0900

    실험 1

---
+++ .netlify/server/nodes/4.js
@@ -0,0 +1,9 @@
+import * as server from '../entries/pages/sverdle/_page.server.js';
+
+export const index = 4;
+export const component = async () => (await import('../entries/pages/sverdle/_page.svelte.js')).default;
+export const file = '_app/immutable/components/pages/sverdle/_page.svelte-69273dd3.js';
+export { server };
+export const imports = ["_app/immutable/components/pages/sverdle/_page.svelte-69273dd3.js","_app/immutable/chunks/index-20c84258.js","_app/immutable/chunks/parse-b67c4dc9.js","_app/immutable/chunks/singletons-317f8e08.js","_app/immutable/chunks/index-295d139c.js"];
+export const stylesheets = ["_app/immutable/assets/_page-9d501049.css"];
+export const fonts = [];
